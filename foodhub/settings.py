d5176f1c9c1525b7f9f729a791e555f3c0b007cf
Urmila Sarvaiya <sanurmi0129@gmail.com>
2026-01-28 22:30:16 +0530

    Added custom user model & configured PostreSQL

--- foodhub/settings.py
+++ foodhub/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
