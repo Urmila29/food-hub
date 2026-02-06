486e23e1e36999c8eac1d536cd33c3e63742d734
Urmila Sarvaiya <sanurmi0129@gmail.com>
2026-02-06 19:36:15 +0530

    User roles and groups configured

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin, Group
 
 
 class MyUserManager(BaseUserManager):
@@ -15,11 +15,21 @@ class MyUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        # Force the role to ADMIN for superusers
+        extra_fields.setdefault('role', 'ADMIN')
+        
         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        VENDOR = "VENDOR", "Vendor"
+        STAFF = "STAFF", "Staff"
+        CUSTOMER = "CUSTOMER", "Customer"
+
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.CUSTOMER)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
@@ -30,6 +40,15 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email' # Use email to login instead of username
     REQUIRED_FIELDS = []     # Email & password are required by default
+
+    def save(self, *args, **kwargs):
+        # 1. Save the user first so they have an ID
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+
+        # 2. Assign to the correct Django Group
+        group, created = Group.objects.get_or_create(name=self.role)
+        self.groups.add(group)
 
     def __str__(self):
         return self.email
